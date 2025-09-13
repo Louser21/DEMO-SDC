2d5a305a44492d37aeecf0c23da308e3113541ea
Nitin Bhandari <nitin211206@gmail.com>
2025-09-13 15:30:41 +0530

    feat: vercel

--- backend/index.js
+++ backend/index.js
@@ -51,15 +51,11 @@ app.use('/api', apiRouter);
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-mongoose.connect(MONGODB_URL)
-  .then(() => {
-    console.log('Connected to MongoDB successfully');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB:", error.message)
+  );
 
   module.exports = app;
